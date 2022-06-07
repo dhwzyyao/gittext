b6be9b135dc7654bc0f0411116ded4ab93f6dab4
wzy <461027327@qq.com>
2022-06-07 09:46:03 +0800

    third commit

--- src/main/java/com/wzy/study/GitStudy.java
+++ src/main/java/com/wzy/study/GitStudy.java
@@ -7,5 +7,7 @@ package com.wzy.study;
 public class GitStudy {
     public static void main(String[] args) {
         System.out.println("dd");
+        System.out.println("dd");
+        System.out.println("dd2");
     }
 }
